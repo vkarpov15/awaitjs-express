092fe692e674a0394160c218514da1611a8ecb4f
Valeri Karpov <val@karpov.io>
2021-10-22 15:33:24 -0400

    fix(index.d.ts): add workaround for TS linters that enforce no-misused-promises
    Fix #28
    Fix #30

--- index.d.ts
+++ index.d.ts
@@ -1,28 +1,31 @@
-import { Express, Router, IRouter, RouterOptions } from 'express'
+import { Express, Router, IRouter, RouterOptions, RequestHandler } from 'express';
+
+type PathParams = string | RegExp | Array<string | RegExp>;
+type AsyncRequestHandler = ((path: PathParams, fn: (...args: Parameters<RequestHandler>) => Promise<void>) => void);
 
 export interface IRouterWithAsync {
-  useAsync: IRouter['use']
-  deleteAsync: IRouter['delete']
-  getAsync: IRouter['get']
-  headAsync: IRouter['head']
-  paramAsync: IRouter['param']
-  patchAsync: IRouter['patch']
-  postAsync: IRouter['post']
-  putAsync: IRouter['put']
+  useAsync: IRouter['use'] | AsyncRequestHandler;
+  deleteAsync: IRouter['delete'] | AsyncRequestHandler;
+  getAsync: IRouter['get'] | AsyncRequestHandler;
+  headAsync: IRouter['head'] | AsyncRequestHandler;
+  paramAsync: IRouter['param'] | AsyncRequestHandler;
+  patchAsync: IRouter['patch'] | AsyncRequestHandler;
+  postAsync: IRouter['post'] | AsyncRequestHandler;
+  putAsync: IRouter['put'] | AsyncRequestHandler;
 }
 
 export interface ExpressWithAsync extends Express, IRouterWithAsync { }
 export interface RouterWithAsync extends Router, IRouterWithAsync { }
 
-declare function addAsync(app: Express): ExpressWithAsync
-declare function addAsync(app: Router): RouterWithAsync
-export { addAsync }
+declare function addAsync(app: Express): ExpressWithAsync;
+declare function addAsync(app: Router): RouterWithAsync;
+export { addAsync };
 
-export const decorateRouter: (router: Router) => RouterWithAsync
-export const decorateApp: (router: Express) => ExpressWithAsync
+export const decorateRouter: (router: Router) => RouterWithAsync;
+export const decorateApp: (router: Express) => ExpressWithAsync;
 
-declare function AsyncRouter(options?: RouterOptions): RouterWithAsync
-export { AsyncRouter as Router }
+declare function AsyncRouter(options?: RouterOptions): RouterWithAsync;
+export { AsyncRouter as Router };
 
-declare function wrap(fn: Function, isParam: boolean): Function
-export { wrap }
+declare function wrap(fn: ((...args: Parameters<RequestHandler>) => Promise<void>) | Function, isParam?: boolean): Function;
+export { wrap };
